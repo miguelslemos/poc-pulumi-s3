3d612c0fb55c39153ff222c6f835706c5edd8968
Miguel Lemos <miguelslemos@gmail.com>
2023-09-18 22:40:41 -0300

    Fix bucket policy input

--- projects/add-bucket-policy/main.go
+++ projects/add-bucket-policy/main.go
@@ -9,9 +9,9 @@ import (
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		conf := config.New(ctx, "")
-		bucketName := conf.Require("bucket-name")
+		bucketArn := conf.Require("bucketArn")
 		if _, err := s3outposts.NewBucketPolicy(ctx, "bucketPolicy", &s3outposts.BucketPolicyArgs{
-			Bucket: pulumi.String(bucketName),
+			Bucket: pulumi.String(bucketArn),
 			PolicyDocument: pulumi.Any(map[string]interface{}{
 				"Version": "2012-10-17",
 				"Statement": []map[string]interface{}{
@@ -22,7 +22,7 @@ func main() {
 							"s3:GetObject",
 						},
 						"Resource": []interface{}{
-							pulumi.Sprintf("arn:aws:s3:::%s/lala/*", pulumi.String(bucketName)),
+							pulumi.Sprintf("arn:aws:s3:::%s/lala/*", pulumi.String(bucketArn)),
 						},
 					},
 				},
